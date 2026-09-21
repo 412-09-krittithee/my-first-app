2fbc8828f34f254d8ac72a87f78e3d733818be67
412-09-krittithee <54272@yupparaj.ac.th>
2026-09-21 19:27:12 +0700

    Update Vocab_game.py

--- Vocab_game.py
+++ Vocab_game.py
@@ -52,7 +52,7 @@ def show_result_dialog(ans1, ans2,ans3,ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans3 == "mango
+    if u_ans3 == "mango":
     ":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
